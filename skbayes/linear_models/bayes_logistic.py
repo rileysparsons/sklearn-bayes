dd027e9e709787db71d475680c5a15a8b10af32e
Riley Parsons <riley.s.parsons@gmail.com>
2017-07-27 13:54:48 -0700

    Update bayes_logistic.py

--- skbayes/linear_models/bayes_logistic.py
+++ skbayes/linear_models/bayes_logistic.py
@@ -385,9 +385,9 @@ class VBLogisticRegression(BayesianLogisticRegression):
             # -------- update eps  ------------
             # In the M-step we update parameter eps which controls 
             # accuracy of local variational approximation to lower bound
-            XMX = X.dot(w).power(2)
-            XSX = X.dot(Ri.T).power(2).sum(axis = 1)
-            eps = XMX.sum(XSX).sqrt()
+            XMX = X.dot(w)**2
+            XSX = np.sum(X.dot(Ri.T)**2, axis=1)
+            eps = np.sqrt(XMX+XSX)
             
             # convergence
             if np.sum(abs(w-w0) > self.tol) == 0 or i==self.n_iter-1:
